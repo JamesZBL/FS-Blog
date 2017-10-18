456c7e39e88c975fe73de82a8805a0a573012da7
1146556298@qq.com <1146556298@qq.com>
2017-10-18 22:25:53 +0800

    init

--- src/main/java/me/zbl/fullstack/FullstackApplication.java
+++ src/main/java/me/zbl/fullstack/FullstackApplication.java
@@ -2,11 +2,14 @@ package me.zbl.fullstack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
 
 @SpringBootApplication
+@EnableCaching
 public class FullstackApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FullstackApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FullstackApplication.class, args);
+    }
 }
