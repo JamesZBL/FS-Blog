d325fefc6f6a83233e7115080a4e2eca0db59b4c
1146556298@qq.com <1146556298@qq.com>
2017-10-18 23:54:19 +0800

    init

---
+++ src/main/java/me/zbl/fullstack/conf/WebConfAdapter.java
@@ -0,0 +1,9 @@
+package me.zbl.fullstack.conf;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebConfAdapter extends WebMvcConfigurerAdapter{
+
+}
---
+++ src/main/java/me/zbl/fullstack/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package me.zbl.fullstack.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(value = Exception.class)
+    public String handleException(Exception e, Model model) {
+        if (null != e) {
+            model.addAttribute("msg",e.getLocalizedMessage());
+        }
+        return "/error";
+    }
+}
