9dcf0b28ef625167592a6576cb23a5fe7864b232
1146556298@qq.com <1146556298@qq.com>
2017-10-20 12:49:44 +0800

    改为嵌入式运行环境

--- src/main/java/me/zbl/fullstack/conf/WebConfAdapter.java
+++ src/main/java/me/zbl/fullstack/conf/WebConfAdapter.java
@@ -2,27 +2,28 @@ package me.zbl.fullstack.conf;
 
 import me.zbl.fullstack.interceptor.UserAuthenticationInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.bind.RelaxedPropertyResolver;
-import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.validation.Validator;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.nio.charset.Charset;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * WEB MVC 配置
+ */
 @Configuration
-public class WebConfAdapter extends WebMvcConfigurerAdapter implements EnvironmentAware{
-    private RelaxedPropertyResolver propertyResolver;
+public class WebConfAdapter extends WebMvcConfigurerAdapter{
 
     private UserAuthenticationInterceptor securityInterceptor;
 
@@ -34,27 +35,15 @@ public class WebConfAdapter extends WebMvcConfigurerAdapter implements Environme
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(securityInterceptor).addPathPatterns("/admin/**");//配置登录拦截器拦截路径
+        //后台登录拦截器拦截路径
+        registry.addInterceptor(securityInterceptor).addPathPatterns("/admin/**");
     }
 
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowCredentials(true);
-        configuration.addAllowedOrigin("*");
-        configuration.addAllowedHeader("*");
-        configuration.setAllowedMethods(Arrays.asList("GET", "PUT", "POST", "DELETE"));
-        source.registerCorsConfiguration("/**", configuration);
-        return new CorsFilter(source);
-    }
-
-
-    @Bean
-    public HttpMessageConverter<String> responseBodyConverter() {
-        StringHttpMessageConverter converter = new StringHttpMessageConverter(
-                Charset.forName("UTF-8"));
-        return converter;
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        super.addViewControllers(registry);
+        //主页
+        registry.addViewController("/").setViewName("index");
     }
 
     @Override
@@ -70,7 +59,27 @@ public class WebConfAdapter extends WebMvcConfigurerAdapter implements Environme
     }
 
     @Override
-    public void setEnvironment(Environment env) {
-        this.propertyResolver = new RelaxedPropertyResolver(env, "pic.");
+    public Validator getValidator() {
+        return super.getValidator();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(true);
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedHeader("*");
+        configuration.setAllowedMethods(Arrays.asList("GET", "PUT", "POST", "DELETE"));
+        source.registerCorsConfiguration("/**", configuration);
+        return new CorsFilter(source);
+    }
+
+    @Bean
+    public HttpMessageConverter<String> responseBodyConverter() {
+        //编码
+        StringHttpMessageConverter converter = new StringHttpMessageConverter(
+                Charset.forName("UTF-8"));
+        return converter;
     }
 }
