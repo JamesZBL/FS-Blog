0e5cf3a61761457d7b291431549523713f5f5a60
1146556298@qq.com <1146556298@qq.com>
2017-10-22 19:24:19 +0800

    改为war方式部署

--- src/main/java/me/zbl/fullstack/FullstackApplication.java
+++ src/main/java/me/zbl/fullstack/FullstackApplication.java
@@ -2,15 +2,23 @@ package me.zbl.fullstack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
 @EnableCaching
-public class FullstackApplication {
+public class FullstackApplication  extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(FullstackApplication.class, args);
     }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(this.getClass());
+    }
+
 }
