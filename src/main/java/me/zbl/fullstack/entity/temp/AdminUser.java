cd66b3cdf0941f730efc5a11dc490757fe877a24
1146556298@qq.com <1146556298@qq.com>
2017-10-23 21:44:19 +0800

    对AdminUser和User重新生成之后

---
+++ src/main/java/me/zbl/fullstack/entity/temp/AdminUser.java
@@ -0,0 +1,155 @@
+package me.zbl.fullstack.entity.temp;
+
+import java.util.Date;
+
+public class AdminUser {
+    private Integer id;
+
+    private String username;
+
+    private String password;
+
+    private Date birthDate;
+
+    private String nickname;
+
+    private String email;
+
+    private String github;
+
+    private String qq;
+
+    private String wecaht;
+
+    private String avatar;
+
+    private Date gmtCreate;
+
+    private Date gmtModified;
+
+    private String hobby;
+
+    private String motto;
+
+    private String mdInfo;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname == null ? null : nickname.trim();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+
+    public String getGithub() {
+        return github;
+    }
+
+    public void setGithub(String github) {
+        this.github = github == null ? null : github.trim();
+    }
+
+    public String getQq() {
+        return qq;
+    }
+
+    public void setQq(String qq) {
+        this.qq = qq == null ? null : qq.trim();
+    }
+
+    public String getWecaht() {
+        return wecaht;
+    }
+
+    public void setWecaht(String wecaht) {
+        this.wecaht = wecaht == null ? null : wecaht.trim();
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar == null ? null : avatar.trim();
+    }
+
+    public Date getGmtCreate() {
+        return gmtCreate;
+    }
+
+    public void setGmtCreate(Date gmtCreate) {
+        this.gmtCreate = gmtCreate;
+    }
+
+    public Date getGmtModified() {
+        return gmtModified;
+    }
+
+    public void setGmtModified(Date gmtModified) {
+        this.gmtModified = gmtModified;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby == null ? null : hobby.trim();
+    }
+
+    public String getMotto() {
+        return motto;
+    }
+
+    public void setMotto(String motto) {
+        this.motto = motto == null ? null : motto.trim();
+    }
+
+    public String getMdInfo() {
+        return mdInfo;
+    }
+
+    public void setMdInfo(String mdInfo) {
+        this.mdInfo = mdInfo == null ? null : mdInfo.trim();
+    }
+}
