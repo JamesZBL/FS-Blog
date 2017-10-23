937bdb2c5488753c0eff4c1d7d52d2aaeebae1dd
1146556298@qq.com <1146556298@qq.com>
2017-10-23 21:51:56 +0800

    修改AdminUser和User的mapper后

--- src/main/java/me/zbl/fullstack/mapper/AdminUserMapper.java
+++ src/main/java/me/zbl/fullstack/mapper/AdminUserMapper.java
@@ -16,100 +16,92 @@ public interface AdminUserMapper {
     int deleteByPrimaryKey(Integer id);
 
     @Insert({
-        "insert into admin_user (username, password, ",
-        "nickname, email, ",
-        "github, qq, wecaht, ",
-        "avatar, bir_year, ",
-        "bir_month, bir_day, ",
-        "gmt_create, gmt_modified, ",
-        "hobby, motto, ",
-        "md_info)",
-        "values (#{username,jdbcType=VARCHAR}, #{password,jdbcType=VARCHAR}, ",
-        "#{nickname,jdbcType=VARCHAR}, #{email,jdbcType=VARCHAR}, ",
-        "#{github,jdbcType=VARCHAR}, #{qq,jdbcType=VARCHAR}, #{wecaht,jdbcType=VARCHAR}, ",
-        "#{avatar,jdbcType=VARCHAR}, #{birYear,jdbcType=INTEGER}, ",
-        "#{birMonth,jdbcType=INTEGER}, #{birDay,jdbcType=INTEGER}, ",
-        "#{gmtCreate,jdbcType=TIMESTAMP}, #{gmtModified,jdbcType=TIMESTAMP}, ",
-        "#{hobby,jdbcType=LONGVARCHAR}, #{motto,jdbcType=LONGVARCHAR}, ",
-        "#{mdInfo,jdbcType=LONGVARCHAR})"
+            "insert into admin_user (username, password, ",
+            "birth_date, nickname, ",
+            "email, github, qq, ",
+            "wecaht, avatar, ",
+            "gmt_create, gmt_modified, ",
+            "hobby, motto, ",
+            "md_info)",
+            "values (#{username,jdbcType=VARCHAR}, MD5(#{password,jdbcType=VARCHAR}), ",
+            "#{birthDate,jdbcType=DATE}, #{nickname,jdbcType=VARCHAR}, ",
+            "#{email,jdbcType=VARCHAR}, #{github,jdbcType=VARCHAR}, #{qq,jdbcType=VARCHAR}, ",
+            "#{wecaht,jdbcType=VARCHAR}, #{avatar,jdbcType=VARCHAR}, ",
+            "#{gmtCreate,jdbcType=TIMESTAMP}, #{gmtModified,jdbcType=TIMESTAMP}, ",
+            "#{hobby,jdbcType=LONGVARCHAR}, #{motto,jdbcType=LONGVARCHAR}, ",
+            "#{mdInfo,jdbcType=LONGVARCHAR})"
     })
     @SelectKey(statement="SELECT LAST_INSERT_ID()", keyProperty="id", before=false, resultType=Integer.class)
     int insert(AdminUser record);
 
     @Select({
-        "select",
-        "id, username, password, nickname, email, github, qq, wecaht, avatar, bir_year, ",
-        "bir_month, bir_day, gmt_create, gmt_modified, hobby, motto, md_info",
-        "from admin_user",
-        "where id = #{id,jdbcType=INTEGER}"
+            "select",
+            "id, username, password, birth_date, nickname, email, github, qq, wecaht, avatar, ",
+            "gmt_create, gmt_modified, hobby, motto, md_info",
+            "from admin_user",
+            "where id = #{id,jdbcType=INTEGER}"
     })
     @Results({
-        @Result(column="id", property="id", jdbcType=JdbcType.INTEGER, id=true),
-        @Result(column="username", property="username", jdbcType=JdbcType.VARCHAR),
-        @Result(column="password", property="password", jdbcType=JdbcType.VARCHAR),
-        @Result(column="nickname", property="nickname", jdbcType=JdbcType.VARCHAR),
-        @Result(column="email", property="email", jdbcType=JdbcType.VARCHAR),
-        @Result(column="github", property="github", jdbcType=JdbcType.VARCHAR),
-        @Result(column="qq", property="qq", jdbcType=JdbcType.VARCHAR),
-        @Result(column="wecaht", property="wecaht", jdbcType=JdbcType.VARCHAR),
-        @Result(column="avatar", property="avatar", jdbcType=JdbcType.VARCHAR),
-        @Result(column="bir_year", property="birYear", jdbcType=JdbcType.INTEGER),
-        @Result(column="bir_month", property="birMonth", jdbcType=JdbcType.INTEGER),
-        @Result(column="bir_day", property="birDay", jdbcType=JdbcType.INTEGER),
-        @Result(column="gmt_create", property="gmtCreate", jdbcType=JdbcType.TIMESTAMP),
-        @Result(column="gmt_modified", property="gmtModified", jdbcType=JdbcType.TIMESTAMP),
-        @Result(column="hobby", property="hobby", jdbcType=JdbcType.LONGVARCHAR),
-        @Result(column="motto", property="motto", jdbcType=JdbcType.LONGVARCHAR),
-        @Result(column="md_info", property="mdInfo", jdbcType=JdbcType.LONGVARCHAR)
+            @Result(column="id", property="id", jdbcType=JdbcType.INTEGER, id=true),
+            @Result(column="username", property="username", jdbcType=JdbcType.VARCHAR),
+            @Result(column="password", property="password", jdbcType=JdbcType.VARCHAR),
+            @Result(column="birth_date", property="birthDate", jdbcType=JdbcType.DATE),
+            @Result(column="nickname", property="nickname", jdbcType=JdbcType.VARCHAR),
+            @Result(column="email", property="email", jdbcType=JdbcType.VARCHAR),
+            @Result(column="github", property="github", jdbcType=JdbcType.VARCHAR),
+            @Result(column="qq", property="qq", jdbcType=JdbcType.VARCHAR),
+            @Result(column="wecaht", property="wecaht", jdbcType=JdbcType.VARCHAR),
+            @Result(column="avatar", property="avatar", jdbcType=JdbcType.VARCHAR),
+            @Result(column="gmt_create", property="gmtCreate", jdbcType=JdbcType.TIMESTAMP),
+            @Result(column="gmt_modified", property="gmtModified", jdbcType=JdbcType.TIMESTAMP),
+            @Result(column="hobby", property="hobby", jdbcType=JdbcType.LONGVARCHAR),
+            @Result(column="motto", property="motto", jdbcType=JdbcType.LONGVARCHAR),
+            @Result(column="md_info", property="mdInfo", jdbcType=JdbcType.LONGVARCHAR)
     })
     AdminUser selectByPrimaryKey(Integer id);
 
     @Select({
-        "select",
-        "id, username, password, nickname, email, github, qq, wecaht, avatar, bir_year, ",
-        "bir_month, bir_day, gmt_create, gmt_modified, hobby, motto, md_info",
-        "from admin_user"
+            "select",
+            "id, username, password, birth_date, nickname, email, github, qq, wecaht, avatar, ",
+            "gmt_create, gmt_modified, hobby, motto, md_info",
+            "from admin_user"
     })
     @Results({
-        @Result(column="id", property="id", jdbcType=JdbcType.INTEGER, id=true),
-        @Result(column="username", property="username", jdbcType=JdbcType.VARCHAR),
-        @Result(column="password", property="password", jdbcType=JdbcType.VARCHAR),
-        @Result(column="nickname", property="nickname", jdbcType=JdbcType.VARCHAR),
-        @Result(column="email", property="email", jdbcType=JdbcType.VARCHAR),
-        @Result(column="github", property="github", jdbcType=JdbcType.VARCHAR),
-        @Result(column="qq", property="qq", jdbcType=JdbcType.VARCHAR),
-        @Result(column="wecaht", property="wecaht", jdbcType=JdbcType.VARCHAR),
-        @Result(column="avatar", property="avatar", jdbcType=JdbcType.VARCHAR),
-        @Result(column="bir_year", property="birYear", jdbcType=JdbcType.INTEGER),
-        @Result(column="bir_month", property="birMonth", jdbcType=JdbcType.INTEGER),
-        @Result(column="bir_day", property="birDay", jdbcType=JdbcType.INTEGER),
-        @Result(column="gmt_create", property="gmtCreate", jdbcType=JdbcType.TIMESTAMP),
-        @Result(column="gmt_modified", property="gmtModified", jdbcType=JdbcType.TIMESTAMP),
-        @Result(column="hobby", property="hobby", jdbcType=JdbcType.LONGVARCHAR),
-        @Result(column="motto", property="motto", jdbcType=JdbcType.LONGVARCHAR),
-        @Result(column="md_info", property="mdInfo", jdbcType=JdbcType.LONGVARCHAR)
+            @Result(column="id", property="id", jdbcType=JdbcType.INTEGER, id=true),
+            @Result(column="username", property="username", jdbcType=JdbcType.VARCHAR),
+            @Result(column="password", property="password", jdbcType=JdbcType.VARCHAR),
+            @Result(column="birth_date", property="birthDate", jdbcType=JdbcType.DATE),
+            @Result(column="nickname", property="nickname", jdbcType=JdbcType.VARCHAR),
+            @Result(column="email", property="email", jdbcType=JdbcType.VARCHAR),
+            @Result(column="github", property="github", jdbcType=JdbcType.VARCHAR),
+            @Result(column="qq", property="qq", jdbcType=JdbcType.VARCHAR),
+            @Result(column="wecaht", property="wecaht", jdbcType=JdbcType.VARCHAR),
+            @Result(column="avatar", property="avatar", jdbcType=JdbcType.VARCHAR),
+            @Result(column="gmt_create", property="gmtCreate", jdbcType=JdbcType.TIMESTAMP),
+            @Result(column="gmt_modified", property="gmtModified", jdbcType=JdbcType.TIMESTAMP),
+            @Result(column="hobby", property="hobby", jdbcType=JdbcType.LONGVARCHAR),
+            @Result(column="motto", property="motto", jdbcType=JdbcType.LONGVARCHAR),
+            @Result(column="md_info", property="mdInfo", jdbcType=JdbcType.LONGVARCHAR)
     })
     List<AdminUser> selectAll();
 
     @Update({
-        "update admin_user",
-        "set username = #{username,jdbcType=VARCHAR},",
-          "password = #{password,jdbcType=VARCHAR},",
-          "nickname = #{nickname,jdbcType=VARCHAR},",
-          "email = #{email,jdbcType=VARCHAR},",
-          "github = #{github,jdbcType=VARCHAR},",
-          "qq = #{qq,jdbcType=VARCHAR},",
-          "wecaht = #{wecaht,jdbcType=VARCHAR},",
-          "avatar = #{avatar,jdbcType=VARCHAR},",
-          "bir_year = #{birYear,jdbcType=INTEGER},",
-          "bir_month = #{birMonth,jdbcType=INTEGER},",
-          "bir_day = #{birDay,jdbcType=INTEGER},",
-          "gmt_create = #{gmtCreate,jdbcType=TIMESTAMP},",
-          "gmt_modified = #{gmtModified,jdbcType=TIMESTAMP},",
-          "hobby = #{hobby,jdbcType=LONGVARCHAR},",
-          "motto = #{motto,jdbcType=LONGVARCHAR},",
-          "md_info = #{mdInfo,jdbcType=LONGVARCHAR}",
-        "where id = #{id,jdbcType=INTEGER}"
+            "update admin_user",
+            "set username = #{username,jdbcType=VARCHAR},",
+            "password = MD5(#{password,jdbcType=VARCHAR}),",
+            "birth_date = #{birthDate,jdbcType=DATE},",
+            "nickname = #{nickname,jdbcType=VARCHAR},",
+            "email = #{email,jdbcType=VARCHAR},",
+            "github = #{github,jdbcType=VARCHAR},",
+            "qq = #{qq,jdbcType=VARCHAR},",
+            "wecaht = #{wecaht,jdbcType=VARCHAR},",
+            "avatar = #{avatar,jdbcType=VARCHAR},",
+            "gmt_create = #{gmtCreate,jdbcType=TIMESTAMP},",
+            "gmt_modified = #{gmtModified,jdbcType=TIMESTAMP},",
+            "hobby = #{hobby,jdbcType=LONGVARCHAR},",
+            "motto = #{motto,jdbcType=LONGVARCHAR},",
+            "md_info = #{mdInfo,jdbcType=LONGVARCHAR}",
+            "where id = #{id,jdbcType=INTEGER}"
     })
     int updateByPrimaryKey(AdminUser record);
 }
--- src/main/java/me/zbl/fullstack/mapper/UserMapper.java
+++ src/main/java/me/zbl/fullstack/mapper/UserMapper.java
@@ -19,88 +19,104 @@ public interface UserMapper {
 
     @Insert({
             "insert into user (username, password, ",
-            "nickname, email, ",
-            "github, qq, wecaht, ",
-            "avatar, bir_year, ",
-            "bir_month, bir_day, ",
+            "birth_date, nickname, ",
+            "email, github, qq, ",
+            "wecaht, avatar, ",
             "status, level, gmt_create, ",
             "gmt_modified, hobby, ",
             "motto, md_info)",
-            "values (#{username,jdbcType=VARCHAR}, MD5(#{password,jdbcType=VARCHAR}), ",
-            "#{nickname,jdbcType=VARCHAR}, #{email,jdbcType=VARCHAR}, ",
-            "#{github,jdbcType=VARCHAR}, #{qq,jdbcType=VARCHAR}, #{wecaht,jdbcType=VARCHAR}, ",
-            "#{avatar,jdbcType=VARCHAR}, #{birYear,jdbcType=INTEGER}, ",
-            "#{birMonth,jdbcType=INTEGER}, #{birDay,jdbcType=INTEGER}, ",
+            "values (#{username,jdbcType=VARCHAR}, #{password,jdbcType=VARCHAR}, ",
+            "#{birthDate,jdbcType=DATE}, #{nickname,jdbcType=VARCHAR}, ",
+            "#{email,jdbcType=VARCHAR}, #{github,jdbcType=VARCHAR}, #{qq,jdbcType=VARCHAR}, ",
+            "#{wecaht,jdbcType=VARCHAR}, #{avatar,jdbcType=VARCHAR}, ",
             "#{status,jdbcType=INTEGER}, #{level,jdbcType=INTEGER}, #{gmtCreate,jdbcType=TIMESTAMP}, ",
             "#{gmtModified,jdbcType=TIMESTAMP}, #{hobby,jdbcType=LONGVARCHAR}, ",
             "#{motto,jdbcType=LONGVARCHAR}, #{mdInfo,jdbcType=LONGVARCHAR})"
     })
-    @SelectKey(statement = "SELECT LAST_INSERT_ID()", keyProperty = "id", before = false, resultType = Integer.class)
-    int insert(User record);
+    @SelectKey(statement="SELECT LAST_INSERT_ID()", keyProperty="id", before=false, resultType=Integer.class)
+    int insert(me.zbl.fullstack.entity.temp.User record);
 
     @Select({
             "select",
-            "id, username, password, nickname, email, github, qq, wecaht, avatar, bir_year, ",
-            "bir_month, bir_day, status, level, gmt_create, gmt_modified, hobby, motto, md_info",
+            "id, username, password, birth_date, nickname, email, github, qq, wecaht, avatar, ",
+            "status, level, gmt_create, gmt_modified, hobby, motto, md_info",
             "from user",
             "where id = #{id,jdbcType=INTEGER}"
     })
     @Results({
-            @Result(column = "id", property = "id", jdbcType = JdbcType.INTEGER, id = true),
-            @Result(column = "username", property = "username", jdbcType = JdbcType.VARCHAR),
-            @Result(column = "password", property = "password", jdbcType = JdbcType.VARCHAR),
-            @Result(column = "nickname", property = "nickname", jdbcType = JdbcType.VARCHAR),
-            @Result(column = "email", property = "email", jdbcType = JdbcType.VARCHAR),
-            @Result(column = "github", property = "github", jdbcType = JdbcType.VARCHAR),
-            @Result(column = "qq", property = "qq", jdbcType = JdbcType.VARCHAR),
-            @Result(column = "wecaht", property = "wecaht", jdbcType = JdbcType.VARCHAR),
-            @Result(column = "avatar", property = "avatar", jdbcType = JdbcType.VARCHAR),
-            @Result(column = "bir_year", property = "birYear", jdbcType = JdbcType.INTEGER),
-            @Result(column = "bir_month", property = "birMonth", jdbcType = JdbcType.INTEGER),
-            @Result(column = "bir_day", property = "birDay", jdbcType = JdbcType.INTEGER),
-            @Result(column = "status", property = "status", jdbcType = JdbcType.INTEGER),
-            @Result(column = "level", property = "level", jdbcType = JdbcType.INTEGER),
-            @Result(column = "gmt_create", property = "gmtCreate", jdbcType = JdbcType.TIMESTAMP),
-            @Result(column = "gmt_modified", property = "gmtModified", jdbcType = JdbcType.TIMESTAMP),
-            @Result(column = "hobby", property = "hobby", jdbcType = JdbcType.LONGVARCHAR),
-            @Result(column = "motto", property = "motto", jdbcType = JdbcType.LONGVARCHAR),
-            @Result(column = "md_info", property = "mdInfo", jdbcType = JdbcType.LONGVARCHAR)
+            @Result(column="id", property="id", jdbcType=JdbcType.INTEGER, id=true),
+            @Result(column="username", property="username", jdbcType=JdbcType.VARCHAR),
+            @Result(column="password", property="password", jdbcType=JdbcType.VARCHAR),
+            @Result(column="birth_date", property="birthDate", jdbcType=JdbcType.DATE),
+            @Result(column="nickname", property="nickname", jdbcType=JdbcType.VARCHAR),
+            @Result(column="email", property="email", jdbcType=JdbcType.VARCHAR),
+            @Result(column="github", property="github", jdbcType=JdbcType.VARCHAR),
+            @Result(column="qq", property="qq", jdbcType=JdbcType.VARCHAR),
+            @Result(column="wecaht", property="wecaht", jdbcType=JdbcType.VARCHAR),
+            @Result(column="avatar", property="avatar", jdbcType=JdbcType.VARCHAR),
+            @Result(column="status", property="status", jdbcType=JdbcType.INTEGER),
+            @Result(column="level", property="level", jdbcType=JdbcType.INTEGER),
+            @Result(column="gmt_create", property="gmtCreate", jdbcType=JdbcType.TIMESTAMP),
+            @Result(column="gmt_modified", property="gmtModified", jdbcType=JdbcType.TIMESTAMP),
+            @Result(column="hobby", property="hobby", jdbcType=JdbcType.LONGVARCHAR),
+            @Result(column="motto", property="motto", jdbcType=JdbcType.LONGVARCHAR),
+            @Result(column="md_info", property="mdInfo", jdbcType=JdbcType.LONGVARCHAR)
     })
-    User selectByPrimaryKey(Integer id);
+    me.zbl.fullstack.entity.temp.User selectByPrimaryKey(Integer id);
 
     @Select({
             "select",
-            "id, username, password, nickname, email, github, qq, wecaht, avatar, bir_year, ",
-            "bir_month, bir_day, status, level, gmt_create, gmt_modified, hobby, motto, md_info",
+            "id, username, password, birth_date, nickname, email, github, qq, wecaht, avatar, ",
+            "status, level, gmt_create, gmt_modified, hobby, motto, md_info",
             "from user"
     })
     @Results({
-            @Result(column = "id", property = "id", jdbcType = JdbcType.INTEGER, id = true),
-            @Result(column = "username", property = "username", jdbcType = JdbcType.VARCHAR),
-            @Result(column = "password", property = "password", jdbcType = JdbcType.VARCHAR),
-            @Result(column = "nickname", property = "nickname", jdbcType = JdbcType.VARCHAR),
-            @Result(column = "email", property = "email", jdbcType = JdbcType.VARCHAR),
-            @Result(column = "github", property = "github", jdbcType = JdbcType.VARCHAR),
-            @Result(column = "qq", property = "qq", jdbcType = JdbcType.VARCHAR),
-            @Result(column = "wecaht", property = "wecaht", jdbcType = JdbcType.VARCHAR),
-            @Result(column = "avatar", property = "avatar", jdbcType = JdbcType.VARCHAR),
-            @Result(column = "bir_year", property = "birYear", jdbcType = JdbcType.INTEGER),
-            @Result(column = "bir_month", property = "birMonth", jdbcType = JdbcType.INTEGER),
-            @Result(column = "bir_day", property = "birDay", jdbcType = JdbcType.INTEGER),
-            @Result(column = "status", property = "status", jdbcType = JdbcType.INTEGER),
-            @Result(column = "level", property = "level", jdbcType = JdbcType.INTEGER),
-            @Result(column = "gmt_create", property = "gmtCreate", jdbcType = JdbcType.TIMESTAMP),
-            @Result(column = "gmt_modified", property = "gmtModified", jdbcType = JdbcType.TIMESTAMP),
-            @Result(column = "hobby", property = "hobby", jdbcType = JdbcType.LONGVARCHAR),
-            @Result(column = "motto", property = "motto", jdbcType = JdbcType.LONGVARCHAR),
-            @Result(column = "md_info", property = "mdInfo", jdbcType = JdbcType.LONGVARCHAR)
+            @Result(column="id", property="id", jdbcType=JdbcType.INTEGER, id=true),
+            @Result(column="username", property="username", jdbcType=JdbcType.VARCHAR),
+            @Result(column="password", property="password", jdbcType=JdbcType.VARCHAR),
+            @Result(column="birth_date", property="birthDate", jdbcType=JdbcType.DATE),
+            @Result(column="nickname", property="nickname", jdbcType=JdbcType.VARCHAR),
+            @Result(column="email", property="email", jdbcType=JdbcType.VARCHAR),
+            @Result(column="github", property="github", jdbcType=JdbcType.VARCHAR),
+            @Result(column="qq", property="qq", jdbcType=JdbcType.VARCHAR),
+            @Result(column="wecaht", property="wecaht", jdbcType=JdbcType.VARCHAR),
+            @Result(column="avatar", property="avatar", jdbcType=JdbcType.VARCHAR),
+            @Result(column="status", property="status", jdbcType=JdbcType.INTEGER),
+            @Result(column="level", property="level", jdbcType=JdbcType.INTEGER),
+            @Result(column="gmt_create", property="gmtCreate", jdbcType=JdbcType.TIMESTAMP),
+            @Result(column="gmt_modified", property="gmtModified", jdbcType=JdbcType.TIMESTAMP),
+            @Result(column="hobby", property="hobby", jdbcType=JdbcType.LONGVARCHAR),
+            @Result(column="motto", property="motto", jdbcType=JdbcType.LONGVARCHAR),
+            @Result(column="md_info", property="mdInfo", jdbcType=JdbcType.LONGVARCHAR)
     })
-    List<User> selectAll();
+    List<me.zbl.fullstack.entity.temp.User> selectAll();
+
+    @Update({
+            "update user",
+            "set username = #{username,jdbcType=VARCHAR},",
+            "password = #{password,jdbcType=VARCHAR},",
+            "birth_date = #{birthDate,jdbcType=DATE},",
+            "nickname = #{nickname,jdbcType=VARCHAR},",
+            "email = #{email,jdbcType=VARCHAR},",
+            "github = #{github,jdbcType=VARCHAR},",
+            "qq = #{qq,jdbcType=VARCHAR},",
+            "wecaht = #{wecaht,jdbcType=VARCHAR},",
+            "avatar = #{avatar,jdbcType=VARCHAR},",
+            "status = #{status,jdbcType=INTEGER},",
+            "level = #{level,jdbcType=INTEGER},",
+            "gmt_create = #{gmtCreate,jdbcType=TIMESTAMP},",
+            "gmt_modified = #{gmtModified,jdbcType=TIMESTAMP},",
+            "hobby = #{hobby,jdbcType=LONGVARCHAR},",
+            "motto = #{motto,jdbcType=LONGVARCHAR},",
+            "md_info = #{mdInfo,jdbcType=LONGVARCHAR}",
+            "where id = #{id,jdbcType=INTEGER}"
+    })
+    int updateByPrimaryKey(me.zbl.fullstack.entity.temp.User record);
 
     @Select({
             "select",
-            "id, username, password, nickname, email, github, qq, wecaht, avatar, bir_year, ",
-            "bir_month, bir_day, status, level, gmt_create, gmt_modified, hobby, motto, md_info",
+            "id, username, password, nickname, email, github, qq, wecaht, avatar, ",
+            "birth_date, status, level, gmt_create, gmt_modified, hobby, motto, md_info",
             "from user",
             "where username = #{username,jdbcType=VARCHAR}",
             "and password = MD5(#{password,jdbcType=VARCHAR})"
@@ -115,9 +131,7 @@ public interface UserMapper {
             @Result(column = "qq", property = "qq", jdbcType = JdbcType.VARCHAR),
             @Result(column = "wecaht", property = "wecaht", jdbcType = JdbcType.VARCHAR),
             @Result(column = "avatar", property = "avatar", jdbcType = JdbcType.VARCHAR),
-            @Result(column = "bir_year", property = "birYear", jdbcType = JdbcType.INTEGER),
-            @Result(column = "bir_month", property = "birMonth", jdbcType = JdbcType.INTEGER),
-            @Result(column = "bir_day", property = "birDay", jdbcType = JdbcType.INTEGER),
+            @Result(column="birth_date", property="birthDate", jdbcType=JdbcType.DATE),
             @Result(column = "status", property = "status", jdbcType = JdbcType.INTEGER),
             @Result(column = "level", property = "level", jdbcType = JdbcType.INTEGER),
             @Result(column = "gmt_create", property = "gmtCreate", jdbcType = JdbcType.TIMESTAMP),
@@ -127,28 +141,4 @@ public interface UserMapper {
             @Result(column = "md_info", property = "mdInfo", jdbcType = JdbcType.LONGVARCHAR)
     })
     List<User> selectByNameAndPassword(UserLoginForm loginForm);
-
-    @Update({
-            "update user",
-            "set username = #{username,jdbcType=VARCHAR},",
-            "password = MD5(#{password,jdbcType=VARCHAR}),",
-            "nickname = #{nickname,jdbcType=VARCHAR},",
-            "email = #{email,jdbcType=VARCHAR},",
-            "github = #{github,jdbcType=VARCHAR},",
-            "qq = #{qq,jdbcType=VARCHAR},",
-            "wecaht = #{wecaht,jdbcType=VARCHAR},",
-            "avatar = #{avatar,jdbcType=VARCHAR},",
-            "bir_year = #{birYear,jdbcType=INTEGER},",
-            "bir_month = #{birMonth,jdbcType=INTEGER},",
-            "bir_day = #{birDay,jdbcType=INTEGER},",
-            "status = #{status,jdbcType=INTEGER},",
-            "level = #{level,jdbcType=INTEGER},",
-            "gmt_create = #{gmtCreate,jdbcType=TIMESTAMP},",
-            "gmt_modified = #{gmtModified,jdbcType=TIMESTAMP},",
-            "hobby = #{hobby,jdbcType=LONGVARCHAR},",
-            "motto = #{motto,jdbcType=LONGVARCHAR},",
-            "md_info = #{mdInfo,jdbcType=LONGVARCHAR}",
-            "where id = #{id,jdbcType=INTEGER}"
-    })
-    int updateByPrimaryKey(User record);
 }
