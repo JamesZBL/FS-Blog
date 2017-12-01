cbc202439911208601a305cc787681bc6cb172b8
James <1146556298@qq.com>
2017-12-01 17:56:31 +0800

    去除无用导入

--- src/main/java/me/zbl/fullstack/FullstackApplication.java
+++ src/main/java/me/zbl/fullstack/FullstackApplication.java
@@ -1,8 +1,5 @@
 package me.zbl.fullstack;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
