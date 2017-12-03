e4d4d224d4c5205f7a2fc2fe46eb4c29dbf09fe4
James <1146556298@qq.com>
2017-12-03 22:33:36 +0800

    mapper 去除 @Repository 注解

--- src/main/java/me/zbl/fullstack/mapper/AdminUserMapper.java
+++ src/main/java/me/zbl/fullstack/mapper/AdminUserMapper.java
@@ -2,7 +2,6 @@ package me.zbl.fullstack.mapper;
 
 import me.zbl.fullstack.entity.AdminUser;
 import me.zbl.fullstack.framework.mapper.IMyMapper;
-import org.springframework.stereotype.Repository;
 
 public interface AdminUserMapper extends IMyMapper<AdminUser> {
 
